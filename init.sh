3d012577e475e2f2ac5dae05800bc11f5b7e82ea
manubocquet <manu.bocquet@gmail.com>
2016-10-11 09:54:30 -0400

    first beta

--- init.sh
+++ init.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
+# Set /config/etc
+
 if [ ! -d "/config/etc" ]; then
 	mkdir -p /config/etc 
 fi
 
-if [ ! -d "/config/log" ]; then
-	mkdir -p /config/log
+if [[ ! -f "/config/etc/squid.conf" && -f "/etc/squid3/squid.conf" ]]; then 
+	mv /etc/squid3/squid.conf /config/etc/
 fi
 
-if [ -f "/etc/squid3/squid.conf" ]; then
-	mv /etc/squid3/squid.conf /config/etc/ && \
+if [ -f "/config/etc/squid.conf" ]; then
+	rm /etc/squid3/squid.conf
 	ln -s /config/etc/squid.conf /etc/squid3/squid.conf  
 fi
 
-if [ -d "/var/log/squid3" ]; then
-	mv /var/log/squid3 /config/log && \
+# Set /config/log
+
+if [[ ! -d "/config/log" && -d "/var/log/squid3" ]]; then
+	mv /var/log/squid3 /config/log
+fi
+
+if [ ! -h "/var/log/squid3" ]; then
+	rm -rf /var/log/squid3
 	ln -s /config/log /var/log/squid3
 fi
+
+# Launch Squid3
 
 /usr/sbin/squid3 -N
